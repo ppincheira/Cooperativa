3217d2131414b6d1db4dc5ad841ffc84a7a9609c
Pablo <pincheirapabloj@gmail.com>
2017-11-03 18:42:24 -0300

    Buscador + Funcionalidad
    Agregue al buscador fecha y estado

--- Cooperativa/Controles/forms/frmBuscador.cs
+++ Cooperativa/Controles/forms/frmBuscador.cs
@@ -20,6 +20,8 @@ namespace Controles.forms
         #region << PROPIEDADES >>
         private string _Tabla;
         private string _Campo;
+        private string _filtroCampos;
+        private string _filtroValores;
         private DataTable _dtCombo;
         #endregion
         #region << EVENTOS >>
@@ -44,6 +46,45 @@ namespace Controles.forms
 
         }
 
+    
+
+
+        private void dtpFechaDesde_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                CargarGrilla();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void dtpFechaHasta_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                CargarGrilla();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void cmbEstado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                CargarGrilla();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
         private void frmBuscador_Load(object sender, EventArgs e)
         {
             inicializar();
@@ -55,6 +96,9 @@ namespace Controles.forms
 
             try
             {
+               
+                _filtroCampos = "";
+                _filtroValores = "";
                 _dtCombo = new DataTable();
                 _dtCombo.Columns.Add("DctColumna", typeof(string));
                 _dtCombo.Columns.Add("DctDescripcion", typeof(string));
@@ -62,14 +106,29 @@ namespace Controles.forms
                 List<DetallesColumnasTablas> ListDetalle = oDetalleBus.DetallesColumnasTablasGetByName(_Tabla);
                 foreach (DetallesColumnasTablas oDetalle in ListDetalle)
                 {
-                  
+
                     _Campo = _Campo + ' ' + oDetalle.DctColumna + ' ' + oDetalle.DctDescripcion + ',';
-                    
-                    if (oDetalle.DctFiltroBusqueda == "S")
-                    {
+                    if ((oDetalle.DctFiltroBusqueda == "S") && (oDetalle.DctTipoControl!="Fecha" ) && oDetalle.DctTipoControl != "Estado") { 
                         _dtCombo.Rows.Add(oDetalle.DctColumna, oDetalle.DctDescripcion);
+                     
                     }
 
+                    if ((oDetalle.DctFiltroBusqueda == "S") && (oDetalle.DctTipoControl == "Fecha"))
+                    { 
+                        this.gpbGrupoFecha.Visible = true;
+                        _filtroCampos = oDetalle.DctColumna + "&";
+                        _filtroValores = this.dtpFechaDesde.Text + "%" + this.dtpFechaHasta.Text + "&";
+                    }
+                    if ((oDetalle.DctFiltroBusqueda == "S") && (oDetalle.DctTipoControl == "Estado")) { 
+                        this.gpbGrupoEstado.Visible = true;
+                        _filtroCampos = oDetalle.DctColumna + "&";
+                        _filtroValores = this.cmbEstado.Text + "&";
+                    }
+                }
+
+                if (this.gpbGrupoFecha.Visible) {
+                    this.dtpFechaDesde.Text = DateTime.Now.AddMonths(-1).ToString("dd/MM/yyyy");
+                    this.dtpFechaHasta.Text = DateTime.Now.ToString("dd/MM/yyyy");
                 }
                 this.cmbBuscar.DataSource = _dtCombo;
                 this.cmbBuscar.ValueMember = "DctColumna";
@@ -80,7 +139,7 @@ namespace Controles.forms
 
                
                 TablasBus oTablasBus = new TablasBus();
-                this.dgBusqueda.DataSource = oTablasBus.TablasBusquedaGetAllFilter(_Tabla,_Campo, "", this.txtFiltro.Text);
+                this.dgBusqueda.DataSource = oTablasBus.TablasBusquedaGetAllFilter(_Tabla,_Campo, _filtroCampos, _filtroValores);
                 this.lblCantidad.Text = "Se encontraron " + this.dgBusqueda.VisibleRowCount.ToString() + " registros";
             }
             catch (Exception ex)
@@ -92,8 +151,18 @@ namespace Controles.forms
         }
 
         private void CargarGrilla() {
+
+            
+            if (this.gpbGrupoFecha.Visible)
+                _filtroValores = this.dtpFechaDesde.Text + "%" + this.dtpFechaHasta.Text + "&";
+            if (this.gpbGrupoEstado.Visible)
+                _filtroValores = this.cmbEstado.Text + "&";
+
+            _filtroCampos = this.cmbBuscar.SelectedValue.ToString() + "&";
+            _filtroValores = this.txtFiltro.Text + "&";
+
             TablasBus oTablasBus = new TablasBus();
-            this.dgBusqueda.DataSource = oTablasBus.TablasBusquedaGetAllFilter(_Tabla, _Campo, this.cmbBuscar.SelectedValue.ToString(), this.txtFiltro.Text);
+            this.dgBusqueda.DataSource = oTablasBus.TablasBusquedaGetAllFilter(_Tabla, _Campo, _filtroCampos, _filtroValores);
             this.lblCantidad.Text= "Se encontraron "+this.dgBusqueda.VisibleRowCount.ToString()+ " registros";
 
         }
@@ -105,6 +174,7 @@ namespace Controles.forms
         {
 
         }
+
     }
 
 
