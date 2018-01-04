faff9c89a01a2f4e04fb5f27b461b154542d52d6
karintemi <34041153+karintemi@users.noreply.github.com>
2018-01-04 13:34:43 -0300

    RutasCrud

---
+++ Cooperativa/GesServicios/controles/forms/frmRutasCrud.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FormsAuxiliares
+{
+    partial class frmRutasCrud
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDomiciliosCrud));
+            this.gesGroup2 = new Controles.contenedores.gesGroup();
+            this.btnCancelar = new Controles.buttons.btnCancelar();
+            this.btnAceptar = new Controles.buttons.btnAceptar();
+            this.gbDatos = new Controles.contenedores.gesGroup();
+            this.cmbServicio = new Controles.datos.cmbLista();
+            this.lblParcela = new Controles.labels.lblEtiqueta();
+            this.lblDescripcionCorta = new Controles.labels.lblEtiqueta();
+            this.txtDescripcionCorta = new Controles.textBoxes.txtDescripcionCorta();
+            this.txtDescripcion = new Controles.textBoxes.txtDescripcionCorta();
+            this.lbDescripcion = new Controles.labels.lblEtiqueta();
+            this.gesGroup2.SuspendLayout();
+            this.gbDatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gesGroup2
+            // 
+            this.gesGroup2.Controls.Add(this.btnCancelar);
+            this.gesGroup2.Controls.Add(this.btnAceptar);
+            this.gesGroup2.Location = new System.Drawing.Point(8, 208);
+            this.gesGroup2.Name = "gesGroup2";
+            this.gesGroup2.Size = new System.Drawing.Size(451, 89);
+            this.gesGroup2.TabIndex = 3;
+            this.gesGroup2.TabStop = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.Location = new System.Drawing.Point(234, 19);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(80, 60);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Image = ((System.Drawing.Image)(resources.GetObject("btnAceptar.Image")));
+            this.btnAceptar.Location = new System.Drawing.Point(329, 19);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(80, 60);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // gbDatos
+            // 
+            this.gbDatos.Controls.Add(this.cmbServicio);
+            this.gbDatos.Controls.Add(this.lblParcela);
+            this.gbDatos.Controls.Add(this.lblDescripcionCorta);
+            this.gbDatos.Controls.Add(this.txtDescripcionCorta);
+            this.gbDatos.Controls.Add(this.txtDescripcion);
+            this.gbDatos.Controls.Add(this.lbDescripcion);
+            this.gbDatos.Location = new System.Drawing.Point(8, 3);
+            this.gbDatos.Name = "gbDatos";
+            this.gbDatos.Size = new System.Drawing.Size(451, 185);
+            this.gbDatos.TabIndex = 2;
+            this.gbDatos.TabStop = false;
+            this.gbDatos.Text = "Datos";
+            // 
+            // cmbServicio
+            // 
+            this.cmbServicio.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmbServicio.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbServicio.FormattingEnabled = true;
+            this.cmbServicio.Location = new System.Drawing.Point(149, 111);
+            this.cmbServicio.Name = "cmbServicio";
+            this.cmbServicio.Size = new System.Drawing.Size(165, 21);
+            this.cmbServicio.TabIndex = 33;
+            // 
+            // lblParcela
+            // 
+            this.lblParcela.AutoSize = true;
+            this.lblParcela.Location = new System.Drawing.Point(52, 119);
+            this.lblParcela.Name = "lblParcela";
+            this.lblParcela.Size = new System.Drawing.Size(48, 13);
+            this.lblParcela.TabIndex = 22;
+            this.lblParcela.Text = "Servicio:";
+            // 
+            // lblDescripcionCorta
+            // 
+            this.lblDescripcionCorta.AutoSize = true;
+            this.lblDescripcionCorta.Location = new System.Drawing.Point(52, 87);
+            this.lblDescripcionCorta.Name = "lblDescripcionCorta";
+            this.lblDescripcionCorta.Size = new System.Drawing.Size(91, 13);
+            this.lblDescripcionCorta.TabIndex = 21;
+            this.lblDescripcionCorta.Text = "Descripción Corta";
+            // 
+            // txtDescripcionCorta
+            // 
+            this.txtDescripcionCorta.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDescripcionCorta.Location = new System.Drawing.Point(149, 80);
+            this.txtDescripcionCorta.MaxLength = 20;
+            this.txtDescripcionCorta.Name = "txtDescripcionCorta";
+            this.txtDescripcionCorta.Size = new System.Drawing.Size(165, 20);
+            this.txtDescripcionCorta.TabIndex = 20;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDescripcion.Location = new System.Drawing.Point(149, 46);
+            this.txtDescripcion.MaxLength = 20;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(165, 20);
+            this.txtDescripcion.TabIndex = 14;
+            // 
+            // lbDescripcion
+            // 
+            this.lbDescripcion.AutoSize = true;
+            this.lbDescripcion.Location = new System.Drawing.Point(52, 54);
+            this.lbDescripcion.Name = "lbDescripcion";
+            this.lbDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lbDescripcion.TabIndex = 0;
+            this.lbDescripcion.Text = "Descripción:";
+            this.lbDescripcion.Click += new System.EventHandler(this.lbCalle_Click);
+            // 
+            // frmDomiciliosCrud
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 335);
+            this.Controls.Add(this.gesGroup2);
+            this.Controls.Add(this.gbDatos);
+            this.Name = "frmDomiciliosCrud";
+            this.Text = "frmDomiciliosCrud";
+            this.Load += new System.EventHandler(this.frmRutasCrud_Load);
+            this.gesGroup2.ResumeLayout(false);
+            this.gbDatos.ResumeLayout(false);
+            this.gbDatos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controles.contenedores.gesGroup gesGroup2;
+        private Controles.buttons.btnCancelar btnCancelar;
+        private Controles.buttons.btnAceptar btnAceptar;
+        private Controles.contenedores.gesGroup gbDatos;
+        private Controles.labels.lblEtiqueta lblParcela;
+        private Controles.labels.lblEtiqueta lblDescripcionCorta;
+        private Controles.textBoxes.txtDescripcionCorta txtDescripcionCorta;
+        private Controles.textBoxes.txtDescripcionCorta txtDescripcion;
+        private Controles.labels.lblEtiqueta lbDescripcion;
+        private Controles.datos.cmbLista cmbServicio;
+    }
+}
---
+++ Cooperativa/GesServicios/controles/forms/frmRutasCrud.cs
@@ -0,0 +1,96 @@
+﻿using AppProcesos.gesServicios.frmRutasCrud;
+using Controles.datos;
+using Controles.form;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsAuxiliares
+{
+    public partial class frmRutasCrud: gesForm, IVistaRutasCrud
+    {
+
+        #region << PROPIEDADES >>
+
+        UIRutasCrud _oRutasCrud;
+        Utility oUtil;
+
+        long _SruNumero;
+        string _EstCodigo;
+        long _DomCodigoRegistro;
+        string _TdoCodigo;
+      
+
+
+        #endregion
+        #region Implementation of IVistaRutasCrud
+
+
+        public long sruNumero
+        {
+            get { return _SruNumero; }
+            set { _SruNumero = value; }
+        }
+        public string Descripcion
+        {
+            get { return this.txtDescripcion.Text; }
+            set { this.txtDescripcion.Text = value; }
+        }
+
+        public string DescripcionCorta
+        {
+            get { return this.txtDescripcionCorta.Text; }
+            set { this.txtDescripcionCorta.Text = value; }
+        }
+
+        public string estCodigo
+        {
+            get { return _EstCodigo; }
+            set { _EstCodigo = value; }
+        }
+
+        public cmbLista srvCodigo
+        {
+            get { return this.cmbServicio; }
+            set { this.cmbServicio = value; }
+        }
+        #endregion
+        public frmRutasCrud()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRutasCrud_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbLocalidad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbCalle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
